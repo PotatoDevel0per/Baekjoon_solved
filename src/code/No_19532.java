ab46ab458e2b235064a7da5177158daf22bdcad5
PotatoDevel0per <dlwlgur02@gmail.com>
2025-04-08 12:57:43 +0900

    19532 다시 풀기

--- src/code/No_19532.java
+++ src/code/No_19532.java
@@ -1,10 +1,8 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Scanner;
-import java.util.StringTokenizer;
+package code;
 
-public class Main {
+import java.util.Scanner;
+
+public class No_19532 {
     public static void main(String[] args)  {
         Scanner sacn =  new Scanner(System.in);
 
